c4a09c21c926e7b27e819db287bc9b359c92c44c
Tejaswi Alluri <tejaswi.v.alluri@kp.org>
2019-09-16 12:28:04 -0400

    Added a new get service for rewards. Code refactoring.

--- src/main/java/com/example/fitbit/service/FitBitService.java
+++ src/main/java/com/example/fitbit/service/FitBitService.java
@@ -2,79 +2,68 @@ package com.example.fitbit.service;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 
-import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.example.fitbit.model.UserData;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class FitBitService {
 
-	@Value("${customerdetails.file.path}")
-	private String customerDetailsFilePath;
+  @Value("${datastore.path}")
+  private String dataStorePath;
 
-	@Value("${customerdetails.file.name}")
-	private String customerDetailsFileName;
+  @Value("${rewards.min.file.count}")
+  private Integer minFileCount;
 
-	@Value("${rewards.min.file.count}")
-	private Integer rewardsMinFileCount;
+  @Value("${rewards.min.step.count}")
+  private Integer minStepCount;
 
-	@Value("${rewards.min.step.count}")
-	private Integer rewardsMinStepCount;
+  public String post(UserData userData) {
+    ObjectMapper mapper = new ObjectMapper();
+    String jsonString = "";
+    try {
+      jsonString = mapper.writeValueAsString(userData);
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
+    try {
+      FileWriter file =
+          new FileWriter(dataStorePath + String.valueOf(System.currentTimeMillis()) + ".json");
+      file.write(jsonString);
+      file.flush();
+      file.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    System.out.println("Event created.");
+    return "Activity sync complete";
+  }
 
-	private static String CUSTOMER_ACTIVITY_ADDED = "Customer Activity added successfully.";
-	private static String CUSTOMER_ACTIVITY_ADDED_WITH_REWARD = "Customer Activity added successfully.And the customer is "
-			+ "eligible for reward as his/her step count meets the rewards count.";
-
-	public String savecustomerDetails(UserData userData) {
-
-		try {
-			ObjectMapper mapper = new ObjectMapper();
-			String jsonString = mapper.writeValueAsString(userData);
-
-			FileWriter file = new FileWriter(customerDetailsFilePath + customerDetailsFileName + "-"
-					+ DateTime.now().toString("yyyyMMddHHmmss") + ".json");
-			file.write(jsonString);
-			file.flush();
-			file.close();
-
-			int fileCount = new File(customerDetailsFilePath).list().length;
-			System.out.println("Number of files present : " + fileCount);
-
-			if (fileCount >= rewardsMinFileCount) {
-				final File folder = new File(customerDetailsFilePath);
-				int stepCount = listFilesForFolder(folder);
-				if (stepCount >= rewardsMinStepCount) {
-					return CUSTOMER_ACTIVITY_ADDED_WITH_REWARD;
-				}
-			}
-		} catch (Exception e) {
-			System.out.println("Exception occured (savecustomerDetails):  " + e.getMessage());
-		}
-		return CUSTOMER_ACTIVITY_ADDED;
-	}
-
-	public int listFilesForFolder(final File folder) {
-		Integer stepCount = 0;
-		try {
-			for (final File fileEntry : folder.listFiles()) {
-				if (fileEntry.isDirectory()) {
-					listFilesForFolder(fileEntry);
-				} else {
-					ObjectMapper mapper = new ObjectMapper();
-					File file = new File(customerDetailsFilePath + fileEntry.getName());
-					UserData userData = mapper.readValue(file, UserData.class);
-					System.out.println("userData >>  " + userData.toString());
-					stepCount += userData.getStepsCount();
-				}
-			}
-			System.out.println("Total Step Count " + stepCount);
-		} catch (Exception e) {
-			System.out.println("Exception occured (listFilesForFolder) :" + e.getMessage());
-		}
-		return stepCount;
-	}
+  public String get() {
+    File datastore = new File(dataStorePath);
+    int fileCount = datastore.list().length;
+    System.out.println("Number of data files: " + fileCount);
+    int stepCount = 0;
+    if (fileCount >= minFileCount) {
+      for (final File dataFile : datastore.listFiles()) {
+        File file = dataFile.getAbsoluteFile();
+        UserData userData = new UserData();
+        try {
+          userData = new ObjectMapper().readValue(file, UserData.class);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        System.out.println("userData >>  " + userData.toString());
+        stepCount += userData.getStepsCount();
+      }
+    }
+    if (stepCount > minStepCount)
+      return "Congratulations on completing your health goal. You have a reward.";
+    else return "Health goal is pending completion.";
+  }
 }
